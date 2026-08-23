66d9a4291a650584a5ba10946bbefd1205cd0047
Muhammad Shahzaib <shahzaib823786@gmail.com>
2026-08-24 00:54:18 +0500

    Profile Picture shoudl be inside the dashboard after user logout ?Lets see its work or not

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -64,7 +64,7 @@ export async function login(req, res) {
         res.status(200).json({
             message: "Login successful",
             token: token,
-            user: { id: user._id, name: user.name, email: user.email }
+            user: { id: user._id, name: user.name, email: user.email, avatar: user.avatar }
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
